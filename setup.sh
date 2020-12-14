6ad18aeab63993d4af559d5df8b3678e4397940c
AdixYD <66469532+NT4PS@users.noreply.github.com>
2020-12-14 16:03:28 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+pip install requests
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+pip install mechanize
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+pip install requests bs4
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+pkg install figlet
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+pip2 install requests
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+pip2 install tqdm
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+pip2 install mechanize
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+pkg install ruby -y
+clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Adix Ganteng 3X Biar Work Xixixi:v"
+echo
+gem install lolcat
+clear
+figlet AdixYD | lolcat
+sleep 2
+clear
+figlet AdixYD | lolcat
+sleep 2
+clear
+echo
+echo "╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
+echo "╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
+echo "┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
+echo "┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
+echo "╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
+echo "╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo
+echo
+echo "Langsung ketik python2 Cr4ck.py" | lolcat
